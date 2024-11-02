b340946163fa070778f5f79b369fa808fe2af73b
Jursin <1836059252@qq.com>
2024-11-02 23:09:38 +0800

    更新 Clock.vue

--- src/renderer/src/components/Clock.vue
+++ src/renderer/src/components/Clock.vue
@@ -15,8 +15,8 @@ import moment from 'moment';
 
 // 使用ref来存储当前时间
 const formattedTime = ref('');
-const fontSizeIndex = ref(2); // 默认字体大小为7rem
-const fontSizes = ['3rem', '5rem', '7rem']; //不同分辨率适合使用不同大小的字体
+const fontSizeIndex = ref(2); // 默认字体大小索引
+const fontSizes = ['5rem', '7rem', '9rem']; // 增加更大的字体
 
 // 在页面加载完成后初始化时间
 onMounted(() => {
@@ -31,7 +31,7 @@ function updateTime() {
   formattedTime.value = formatDateTime(now);
 }
 
-// 每隔一分钟更新一次时间
+// 每隔250毫秒更新一次时间
 useIntervalFn(updateTime, 250);
 
 // 切换字体大小的函数
@@ -43,5 +43,7 @@ function cycleFontSize() {
 <style scoped>
 .display-1 {
   font-size: v-bind(fontSizes[fontSizeIndex]); /* 动态绑定字体大小 */
+  margin-top: 40px; /* 增加上方间距 */
+  margin-bottom: 40px; /* 增加下方间距 */
 }
 </style>
