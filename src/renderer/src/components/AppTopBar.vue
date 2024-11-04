8b101ba46314596ffd91a2e07c41699a0399be43
Jursin <1836059252@qq.com>
2024-11-04 11:45:17 +0800

    Update AppTopBar.vue

--- src/renderer/src/components/AppTopBar.vue
+++ src/renderer/src/components/AppTopBar.vue
@@ -3,9 +3,9 @@
     <v-app-bar-title>{{ profileStore.appHeader }}</v-app-bar-title>
 
     <template #append>
-      <div class="ga-4">
-        <v-btn @click="router.push('/mainWindow')">回到主页</v-btn>
-        <v-btn @click="router.push('/about')">关于</v-btn>
+      <div class="button-group">
+        <v-btn @click="router.push('/mainWindow')" class="mr-2">回到主页</v-btn>
+        <v-btn @click="router.push('/about')" class="mr-2">关于</v-btn>
         <v-icon icon="mdi-window-close" @click="ipcHandleExit"></v-icon>
       </div>
     </template>
@@ -21,3 +21,11 @@ const router = useRouter();
 
 const ipcHandleExit = () => window.electron.ipcRenderer.send('prog:exit');
 </script>
+
+<style scoped>
+.button-group {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end; /* 将内容对齐到右侧 */
+}
+</style>
